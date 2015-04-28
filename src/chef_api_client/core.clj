bf5c47b5808675a7b4b9cde97b3434a923748dc0
John Bellone <jbellone@bloomberg.net>
2015-04-28 16:04:54 -0400

    Hack a little bit on the authorization headers.

--- src/chef_api_client/core.clj
+++ src/chef_api_client/core.clj
@@ -7,16 +7,18 @@
             [environ.core :refer [env]]
             [pandect.algo.sha1 :as algo]))
 
-(def ^{:const true} default-headers
+(def ^{:const true} *default-headers*
   {:Content-Type "application/json"
    :Accept "application/json"
-   :X-Chef-Version "12.1.0"
+   :X-Chef-Version "12.2.0"
    :X-Ops-Sign "algorithm=sha1;version=1.0;"})
 
-(def ^:dynamic chef-server-url (env :chef-server-url))
+(def ^:dynamic *chef-server-url* (env :chef-server-url))
 
-(defn path->hashed-path [path]
-  (b64/encode (algo/sha1-bytes path)))
+(defn string->b64-sha1
+  "Return a base64 encoded sha1 bytes of value."
+  [value]
+  (b64/encode (algo/sha1-bytes value)))
 
 (defn make-authorization-headers
   [method request-header]
@@ -25,18 +27,17 @@
         timestamp (:X-Ops-Timestamp request-header)
         userid (:X-Ops-UserId request-header)
         headers (str "Method:" method \newline
-                     "Hashed Path:" (path->hashed-path path) \newline
+                     "Hashed Path:" (string->b64-sha1 path) \newline
                      "X-Ops-Content-Hash:" content-hash \newline
                      "X-Ops-Timestamp" timestamp \newline
                      "X-Ops-UserId:" userid \newline)
-        bytes (b64/encode (algo/sha1-bytes headers))]
-    (loop [x 0 num-headers (/ (alen bytes) 59)]
-      (str headers
-           "X-Ops-Authorization-" (+ x 1) ":" \newline))
-    headers))
+        bytes (string->b64-sha1 headers)]
+    (map (fn [s n]
+           (assoc {} (symbol (str ":X-Ops-Authorization-" (inc n))) s))
+         (partition-all 59 bytes) (range))))
 
-(defn make-authentication-headers [client-name client-key & options]
-  (let [headers (or (:headers options) default-headers)
+(defn make-request-headers [client-name client-key & options]
+  (let [headers (or (:headers options) *default-headers*)
         signing-key (slurp client-key)
         method (:method options)
         host (:host options)
@@ -44,11 +45,11 @@
     (when host (headers :Host host))
     (headers :X-Chef-UserId client-name)
     (headers :X-Ops-Timestamp time/now)
-    (headers :X-Content-Hash (b64/encode (algo/sha1-bytes body)))
-    (make-authorization-headers method )))
+    (headers :X-Content-Hash (string->b64-sha1 body))
+    (reduce merge headers (make-authorization-headers method headers))))
 
 (defn make-request [method endpoint]
-  (http/request {:url (str chef-server-url endpoint)
+  (http/request {:url (str *chef-server-url* endpoint)
                  :method method
                  :keepalive 1000}))
 
