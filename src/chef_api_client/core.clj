3807c30bb46b86b6949ecd9c9651cce9ae91eb82
John Bellone <jbellone@bloomberg.net>
2015-04-28 16:46:03 -0400

    Fix errors with signing authz instead of an hmac with key.

--- src/chef_api_client/core.clj
+++ src/chef_api_client/core.clj
@@ -15,38 +15,46 @@
 
 (def ^:dynamic *chef-server-url* (env :chef-server-url))
 
-(defn string->b64-sha1
+(defn string->b64-sha1-hmac
+  "Return a base64 encoded sha1 hmac of value signed with secret-key."
+  [secret-key value]
+  (b64/encode (algo/sha1-hmac-bytes value secret-key)))
+
+(defn string->b64-hmac
   "Return a base64 encoded sha1 bytes of value."
   [value]
   (b64/encode (algo/sha1-bytes value)))
 
 (defn make-authorization-headers
-  [method request-header]
-  (let [path (:Path request-header)
+  [method secret-key request-header]
+  (let [hashed-path (string->b64-sha1 (:Path request-header))
         content-hash (:X-Content-Hash request-header)
         timestamp (:X-Ops-Timestamp request-header)
         userid (:X-Ops-UserId request-header)
         headers (str "Method:" method \newline
-                     "Hashed Path:" (string->b64-sha1 path) \newline
+                     "Hashed Path:" hashed-path \newline
                      "X-Ops-Content-Hash:" content-hash \newline
                      "X-Ops-Timestamp" timestamp \newline
                      "X-Ops-UserId:" userid \newline)
-        bytes (string->b64-sha1 headers)]
+        bytes (string->b64-sha1-hmac headers secret-key)]
     (map (fn [s n]
            (assoc {} (symbol (str ":X-Ops-Authorization-" (inc n))) s))
          (partition-all 59 bytes) (range))))
 
-(defn make-request-headers [client-name client-key & options]
+(defn make-request-headers
+  "Return"
+  [client-name client-key & options]
   (let [headers (or (:headers options) *default-headers*)
         signing-key (slurp client-key)
-        method (:method options)
+        method (clojure.string/upper-case (:method options))
         host (:host options)
         body (:body options)]
     (when host (headers :Host host))
     (headers :X-Chef-UserId client-name)
     (headers :X-Ops-Timestamp time/now)
     (headers :X-Content-Hash (string->b64-sha1 body))
-    (reduce merge headers (make-authorization-headers method headers))))
+    (reduce merge headers
+            (make-authorization-headers method signing-key headers))))
 
 (defn make-request [method endpoint]
   (http/request {:url (str *chef-server-url* endpoint)
