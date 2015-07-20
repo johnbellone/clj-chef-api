1436eef044d529278b7e781d7d30b44e598c1f34
Lou Scoras <louis.j.scoras@gmail.com>
2015-07-20 15:40:03 -0400

    Docs for util functions

--- src/chef_api_client/util/crypto.clj
+++ src/chef_api_client/util/crypto.clj
@@ -12,28 +12,29 @@
 
 ;;; Hash/Encoding utilities
 
-(def ^{:arglists '([^bytes buffer])
-       :doc "Encode base64 encoded byte-array as a UTF-8 string."}
+(def ^{:arglists '([data])
+       :doc "Encode to base64 as a UTF-8 string."}
   b64-string
   (comp #(String. % "UTF-8") b64/encode))
 
-(def ^{:arglists '([^String value secret-key])
+(def ^{:arglists '([value secret-key])
        :doc "Return a base64 encoded hmac-sha1 token from value. Signed with
             secret-key."}
   hmac-sha
   (comp b64-string algo/sha1-hmac-bytes))
 
-(def ^{:arglists '([^String value])
+(def ^{:arglists '([value])
        :doc "Return a base64 encoded string SHA1 digest from value." }
   digest
   (comp b64-string algo/sha1-bytes))
 
 ;;; BC Crypto
 
-(defn init-providers []
+(defn init-providers "Add BouncyCastle to JCE" []
   (Security/addProvider (BouncyCastleProvider.)))
 
-(defn- pem->bc-pkey
+(defn- ^:nodoc pem->bc-pkey
+  "Internal: Extract private key from the OpenSSL pem file at the given path."
   [path]
   (-> path
 	  (java.io.FileReader.)
@@ -43,6 +44,8 @@
 	  (PrivateKeyFactory/createKey)))
 
 (defn read-pem
+  "Read private key from PEM file and convert to a form usable by JCE. Can be
+  passed to the encrypt function in this namespace."
   [path]
   (let [{:keys [exponent modulus]} (into {} (seq (bean (pem->bc-pkey path))))
         factory (KeyFactory/getInstance "RSA")
@@ -50,7 +53,12 @@
     (.generatePrivate factory spec)))
 
 (defn encrypt
+  "Encrypt the given data s, using the provided private key."
   [s pkey]
+  ;; NOTE: Use of private key and not public is intentional. This is what Chef's
+  ;; Mixlib does rather than using the sign/verify methods. See
+  ;; chef-api-client.util.crypto-test/from-ruby for more details and the
+  ;; corresponding Ruby snipit.
   (let [cipher (doto (Cipher/getInstance "RSA/ECB/PKCS1Padding" "BC")
 				 (.init Cipher/ENCRYPT_MODE pkey))]
     (-> s
