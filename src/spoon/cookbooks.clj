2fb28097977a65436d14d265174dad63a5bd6774
John Bellone <jbellone@bloomberg.net>
2015-08-10 12:24:22 -0400

    Fixes string interpolation for cookbooks endpoint.

--- src/spoon/cookbooks.clj
+++ src/spoon/cookbooks.clj
@@ -6,8 +6,8 @@
 
 (defn get-cookbook [org cookbook version & [options]]
   (if version
-    (client/api-request :get "/organizations/%s/cookbooks/%s" [org cookbook version] options)
+    (client/api-request :get "/organizations/%s/cookbooks/%s/%s" [org cookbook version] options)
     (client/api-request :get "/organizations/%s/cookbooks" [org cookbook] options)))
 
 (defn latest-cookbook [org cookbook & [options]]
-  (client/api-request :get "/organizations/%s/cookbooks/_latest" [org cookbook] options))
+  (client/api-request :get "/organizations/%s/cookbooks/%s/_latest" [org cookbook] options))
