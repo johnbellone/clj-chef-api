90d0dc5aa19aec1f3ae709a003527cc04760dbf9
John Bellone <jbellone@bloomberg.net>
2015-08-11 09:41:57 -0400

    Adds the with-chef-server macro to the REPL env.
    This will help running things in the REPL when the only bit we want to
    modify is the host.

--- dev/user.clj
+++ dev/user.clj
@@ -7,13 +7,17 @@
             [clojure.test :refer [run-tests run-all-tests]]
             [environ.core :refer [env]]))
 
-(def default-info
+(def ^:dynamic *client-config*
   (let [client-name (or (env :chef-client-name) (System/getProperty "user.name"))
         client-key (or (env :chef-client-key) (str (System/getProperty "user.home") "/.chef/" client-name ".pem"))
         chef-server (or (env :chef-server-host) "manage.chef.io")]
     (spoon.core/client-info chef-server client-name client-key)))
 
+(defmacro with-chef-server [chef-server & body]
+  `(binding [*client-config* (assoc *client-config* :chef-server-host chef-server)]
+     ~@body))
+
 (defn flip-node-environment
   "Modifies an existing node's environment."
   [org node environment]
-  (spoon.nodes/update-node-environment org node environment default-info))
+  (spoon.nodes/update-node-environment org node environment *client-config*))
