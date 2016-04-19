916ff917ca28dd44906e48e8b40fa02ff6624da9
Lou Scoras <lscoras1@bloomberg.net>
2016-04-19 10:19:54 -0400

    Deprecate current get-* functions

--- src/spoon/search.clj
+++ src/spoon/search.clj
@@ -1,12 +1,20 @@
 (ns spoon.search
   (:require [spoon.core :as client]))
 
-(defn get-nodes [org & [options]]
-  (client/api-request :get "/organizations/%s/search/node" [org] options))
-
-(defn get-clients [org & [options]]
+(defn
+  ^{:deprecated "0.3.3"}
+  get-nodes
+  [org & [options]]
   (client/api-request :get "/organizations/%s/search/client" [org] options))
 
-(defn get-roles [org & [options]]
-  (client/api-request :get "/organizations/%s/search/role" [org] options))
+(defn
+  ^{:deprecated "0.3.3"}
+  get-clients
+  [org & [options]]
+  (client/api-request :get "/organizations/%s/search/client" [org] options))
 
+(defn
+  ^{:deprecated "0.3.3"}
+  get-roles
+  [org & [options]]
+  (client/api-request :get "/organizations/%s/search/role" [org] options))
