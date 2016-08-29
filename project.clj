f733a40f535d31fa026825c35a8e17d38e623a81
Lou Scoras <lscoras1@bloomberg.net>
2016-08-29 13:53:09 -0400

    Bump next dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.bloomberg.inf/spoon "0.3.5"
+(defproject com.bloomberg.inf/spoon "0.3.6-SNAPSHOT"
   :description "Chef Server API client written in Clojure."
   :url "https://github.com/johnbellone/spoon"
   :license {:name "Apache 2.0"
